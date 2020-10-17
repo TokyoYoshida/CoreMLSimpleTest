69ee388f76a35c53e8ea872a670b208f8285288f
tokyoyoshida <yoshidaforpublic@gmail.com>
2020-10-17 16:25:10 +0900

    first commit

---
+++ CoreMLSimpleTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMLSimpleTest
+//
+//  Created by TokyoYoshida on 2020/10/17.
+//  Copyright © 2020 TokyoYoshida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
