0612b0472b151d8e28c7bfb74d3e4f9ce65bba7d
tokyoyoshida <yoshidaforpublic@gmail.com>
2020-10-17 16:31:57 +0900

    prediction

--- CoreMLSimpleTest/ViewController.swift
+++ CoreMLSimpleTest/ViewController.swift
@@ -7,14 +7,18 @@
 //
 
 import UIKit
+import CoreML
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let model = my_model()
+        let inputArray = try! MLMultiArray([2,3])
+        let inputToModel: my_modelInput = my_modelInput(input1: inputArray)
+        if let prediction = try? model.prediction(input: inputToModel) {
+            print(prediction.digit)
+        }
     }
-
-
 }
 
