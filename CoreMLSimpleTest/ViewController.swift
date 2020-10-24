6244e29294686442a4df725ec916bbb5b62f7b3f
tokyoyoshida <yoshidaforpublic@gmail.com>
2020-10-24 16:31:32 +0900

    integrate tp one viewController

--- CoreMLSimpleTest/ViewController.swift
+++ CoreMLSimpleTest/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func buttonSimpleMLTapped(_ sender: Any) {
         let model = my_model()
         let inputArray = try! MLMultiArray([2,3])
         let inputToModel: my_modelInput = my_modelInput(input1: inputArray)
@@ -20,6 +23,19 @@ class ViewController: UIViewController {
             print(prediction.digit)
             print(prediction.digitProbabilities)
         }
+    }
+
+    @IBAction func buttonSimpleMLWithImageTapped(_ sender: Any) {
+        let model = MobileNetV2()
+        let image = UIImage(named: "neco")
+        guard let buffer = image?.pixelBuffer(width: 224, height: 224) else {fatalError()}
+        let input = MobileNetV2Input(input_1: buffer)
+        
+        let output = try! model.prediction(input: input)
+        print(output)
+        
+        print(output.classLabel)
+        print(output.Identity)
     }
 }
 
